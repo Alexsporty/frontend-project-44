640c7e2b243f10ac9c4deb2bf555becd63e68173
Aleksandr Kuptsov <kuptsov.sasha@mail.ru>
2024-05-13 14:35:30 +0300

    project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
+
 [![Actions Status](https://github.com/Alexsporty/frontend-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Alexsporty/frontend-project-44/actions)
+
+https://asciinema.org/a/659153
--- src/cli.js
+++ src/cli.js
@@ -8,52 +8,23 @@ export default () => {
 
   let minNumber = 1;
   let maxNumber = 20;
-  const randNum1 = Math.floor(
-    Math.random() * (maxNumber - minNumber + 1) + minNumber
-  );
-  console.log("Question: " + randNum1);
-  const answer1 = readlineSync.question("Your answer: ");
-  if (randNum1 % 2 === 0 && answer1 === "yes") {
-    console.log("Correct!");
-  } else if (randNum1 % 2 !== 0 && answer1 === "no") {
-    console.log("Correct!");
-  } else {
-    console.log(
-      `'yes' is wrong answer ;(. Correct answer was 'no'. Let's try again, ${name}!`
-    );
-    return;
-  }
 
-  const randNum2 = Math.floor(
-    Math.random() * (maxNumber - minNumber + 1) + minNumber
-  );
-  console.log("Question: " + randNum2);
-  const answer2 = readlineSync.question("Your answer: ");
-  if (randNum2 % 2 === 0 && answer2 === "yes") {
-    console.log("Correct!");
-  } else if (randNum2 % 2 !== 0 && answer2 === "no") {
-    console.log("Correct!");
-  } else {
-    console.log(
-      `'yes' is wrong answer ;(. Correct answer was 'no'. Let's try again, ${name}!`
+  for (let i = 0; i < 3; i++) {
+    const randNumber = Math.floor(
+      Math.random() * (maxNumber - minNumber + 1) + minNumber
     );
-    return;
-  }
-
-  const randNum3 = Math.floor(
-    Math.random() * (maxNumber - minNumber + 1) + minNumber
-  );
-  console.log("Question: " + randNum3);
-  const answer3 = readlineSync.question("Your answer: ");
-  if (randNum3 % 2 === 0 && answer3 === "yes") {
-    console.log("Correct!");
-  } else if (randNum3 % 2 !== 0 && answer3 === "no") {
-    console.log("Correct!");
-  } else {
-    console.log(
-      `'yes' is wrong answer ;(. Correct answer was 'no'. Let's try again, ${name}!`
-    );
-    return;
+    console.log("Question: " + randNumber);
+    const answer = readlineSync.question("Your answer: ");
+    if (randNumber % 2 === 0 && answer === "yes") {
+      console.log("Correct!");
+    } else if (randNumber % 2 !== 0 && answer === "no") {
+      console.log("Correct!");
+    } else {
+      console.log(
+        `${answer}' is wrong answer ;(. Correct answer was 'no'. Let's try again, ${name}!`
+      );
+      return;
+    }
   }
   console.log(`Congratulations, ${name}!`);
 };
