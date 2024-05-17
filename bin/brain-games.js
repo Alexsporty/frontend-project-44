e989b2e22d8647e13f05bbc364bf331e1323a164
Aleksandr Kuptsov <kuptsov.sasha@mail.ru>
2024-05-17 17:29:20 +0300

    add Prime

---
+++ bin/brain-games.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import Hello from "../src/cli.js";
+
+Hello();
--- src/cli.js
+++ src/cli.js
@@ -4,27 +4,4 @@ export default () => {
   console.log("Welcome to the Brain Games!");
   const name = readlineSync.question("May I have your name? ");
   console.log(`Hello, ${name}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-  let minNumber = 1;
-  let maxNumber = 20;
-  let countRound = 3;
-  for (let i = 0; i < countRound; i++) {
-    const randNumber = Math.floor(
-      Math.random() * (maxNumber - minNumber + 1) + minNumber
-    );
-    console.log("Question: " + randNumber);
-    const answer = readlineSync.question("Your answer: ");
-    if (randNumber % 2 === 0 && answer === "yes") {
-      console.log("Correct!");
-    } else if (randNumber % 2 !== 0 && answer === "no") {
-      console.log("Correct!");
-    } else {
-      console.log(
-        `${answer}' is wrong answer ;(. Correct answer was 'no'. Let's try again, ${name}!`
-      );
-      return;
-    }
-  }
-  console.log(`Congratulations, ${name}!`);
 };
